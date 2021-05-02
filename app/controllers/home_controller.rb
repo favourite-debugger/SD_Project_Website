0be299b28a666c3c2a4e0b6af2c533a997efca3a
Kiyolin Perumal <2115440@students.wits.ac.za>
2021-05-02 19:35:31 +0200

    Hint for future webpages

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,11 @@
 class HomeController < ApplicationController
 
-
+#--------------------------------------------------------------------------------------------------------------
+# before_action :authenticate_admin! Must be added to any additional controllers for other webpages,
+# but DO NOT add to the ApplicationController
 before_action :authenticate_admin!
+#----------------------------------------------------------------------------------------------------------------
+
   def index
   end
 end
