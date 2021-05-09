7884209dde44af4578c811060953c0cd1669f1c8
Kiyolin Perumal <2115440@students.wits.ac.za>
2021-05-10 00:28:22 +0200

    Registration for other admins added. (testing still needs to be done)

--- config/routes.rb
+++ config/routes.rb
@@ -1,21 +1,31 @@
 Rails.application.routes.draw do
   get 'home/index'
-  devise_for :admins
+  devise_for :admins,
+    controllers: {:registrations => "registrations" , :sessions => "sessions"} #That will tell devise to use your custom registrations controller
 
+#Setting up a custom route to that action:-----------------------------------------------------------------------------
+as :admin do
+  get "/register", to: "registrations#new", as: "register"
+  get "/sessions", to: "sessionss#new", as: "session"
+end
+#---------------------------------------------------------------------------------------------------------
 
-
-
+#--------------------------------------------------------------------------------------------------------
 devise_scope :admin do
-
+  #root 'devise/sessions#new'#, as: :unauthenticated_root
   authenticated :admin do
     root 'home#index', as: :authenticated_root
+  #  root 'devise/sessions#new', as: :unauthenticated_root
   end
 
   unauthenticated do
-    #root 'home#index', as: :unauthenticated_root
     root 'devise/sessions#new', as: :unauthenticated_root
   end
 end
+#--------------------------------------------------------------------------------------------------------
+
+
+
   #devise_scope :admin do
   #  root to: "devise/sessions#new"
   #end
