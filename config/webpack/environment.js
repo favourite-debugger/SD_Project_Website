7c2469a17c39625876f2bf725db57b180f258620
exit <naickereston@gmail.com>
2021-05-16 15:29:59 +0200

    webpack, yarn errors...missing entry in devise sessions new

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,20 +1,3 @@
 const { environment } = require('@rails/webpacker')
 
-//=======================
-//This code is for adding bootstrap
-
-const webpack = require("webpack")
-
-environment.plugins.append("Provide", new webpack.ProvidePlugin({
-
-$: 'jquery',
-
-jQuery: 'jquery',
-
-Popper: ['popper.js', 'default']
-
-}))
-
-//====================================
-
 module.exports = environment
