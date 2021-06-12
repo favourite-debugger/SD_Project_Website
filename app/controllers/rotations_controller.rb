1649bfd69dba96c10a8520173df8176588660f78
Kiyolin Perumal <2115440@students.wits.ac.za>
2021-06-12 20:20:49 +0200

    changed rotations to blocks

--- app/controllers/rotations_controller.rb
+++ app/controllers/rotations_controller.rb
@@ -1,5 +1,7 @@
 class RotationsController < ApplicationController
+	before_action :authenticate_admin! #kameron: definition found in application_controller.rb
   before_action :set_rotation, only: %i[ show edit update destroy ]
+  require "csv"
 
   # GET /rotations or /rotations.json
   def index
@@ -22,37 +24,69 @@ class RotationsController < ApplicationController
   # POST /rotations or /rotations.json
   def create
     @rotation = Rotation.new(rotation_params)
+      if ( @rotation[:startDate] < @rotation[:endDate]) #checks if the rotation dates make sense
+        respond_to do |format|
+          if @rotation.save
+            format.html { redirect_to @rotation, notice: "Rotation was successfully created." }
+            format.json { render :show, status: :created, location: @rotation }
+          else
+            format.html { render :new, status: :unprocessable_entity }
+            format.json { render json: @rotation.errors, status: :unprocessable_entity }
+          end
+        end
 
-    respond_to do |format|
-      if @rotation.save
-        format.html { redirect_to @rotation, notice: "Rotation was successfully created." }
-        format.json { render :show, status: :created, location: @rotation }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @rotation.errors, status: :unprocessable_entity }
-      end
-    end
+      else#START Showing alert message for invalid rotation dates---------------------------------------------------------------------------------------------------------------------------------------------------
+        flash.now[:alert] = "Rotation Start Date cannot be after the Rotation End Data. Please choose a Rotation Start Date that is eariler than the Rotation End Date"
+        respond_to do |format|
+            format.html { render :new, status: :unprocessable_entity }
+            format.json { render json: @rotation.errors, status: :unprocessable_entity }
+        end
+      end#END Showing alert message for invalid rotation dates---------------------------------------------------------------------------------------------------------------------------------------------------
+
   end
 
   # PATCH/PUT /rotations/1 or /rotations/1.json
   def update
-    respond_to do |format|
-      if @rotation.update(rotation_params)
-        format.html { redirect_to @rotation, notice: "Rotation was successfully updated." }
-        format.json { render :show, status: :ok, location: @rotation }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @rotation.errors, status: :unprocessable_entity }
+    @temprotation = Rotation.new(rotation_params)
+    if ( @temprotation[:startDate] < @temprotation[:endDate]) #checks if the rotation dates make sense
+      respond_to do |format|
+        if @rotation.update(rotation_params)
+          format.html { redirect_to @rotation, notice: "Rotation was successfully updated." }
+          format.json { render :show, status: :ok, location: @rotation }
+        else
+          format.html { render :edit, status: :unprocessable_entity }
+          format.json { render json: @rotation.errors, status: :unprocessable_entity }
+        end
       end
-    end
+    else#START Showing alert message for invalid rotation dates----------------------------------------------------------------------------------------------------------------------------------------------
+      flash.now[:alert] = "Rotation Start Date cannot be after the Rotation End Data. Please choose a Rotation Start Date that is eariler than the Rotation End Date"
+      respond_to do |format|
+          format.html { render :edit, status: :unprocessable_entity }
+          format.json { render json: @rotation.errors, status: :unprocessable_entity }
+      end
+
+    end#END Showing alert message for invalid rotation dates---------------------------------------------------------------------------------------------------------------------------------------------------
   end
 
   # DELETE /rotations/1 or /rotations/1.json
   def destroy
+
     @rotation.destroy
     respond_to do |format|
       format.html { redirect_to rotations_url, notice: "Rotation was successfully destroyed." }
       format.json { head :no_content }
+    end
+  end
+
+  def import #importing from csv file
+    Rotation.import(params[:file])
+    redirect_to rotations_path, notice: "Rotations Added Successfully"
+  end
+
+  def upload
+    uploaded_file = params[:file]
+    File.open(Rails.root.join('public', 'uploads', uploaded_file.original_filename), 'wb') do |file|
+      file.write(uploaded_file.read)
     end
   end
 
--- app/models/rotation.rb
+++ app/models/rotation.rb
@@ -3,4 +3,12 @@ class Rotation < ApplicationRecord
     belongs_to :programme
     has_many :schedules, :dependent => :delete_all 
 
+    def self.import(file)
+        CSV.foreach(file.path, headers: true) do |row|
+          Rotation.create! row.to_hash
+        end
+      end
+
+
+
 end
