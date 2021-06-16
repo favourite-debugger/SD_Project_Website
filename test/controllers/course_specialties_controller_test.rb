a7be1896987ced04a196b2bd016f7a6a455c9ebe
RXRider1451 <RXRider1451@Gmail.com>
2021-06-16 12:24:47 +0200

    Do we still get 100% coverage?...

--- test/controllers/course_specialties_controller_test.rb
+++ test/controllers/course_specialties_controller_test.rb
@@ -1,7 +1,7 @@
 
 class CourseSpecialtiesControllerTest < ActionDispatch::IntegrationTest
   setup do
-#    @course_specialty = course_specialties(:one)
+    @course_specialty = course_specialties(:one)
   end
 
 end
--- test/controllers/specialties_controller_test.rb
+++ test/controllers/specialties_controller_test.rb
@@ -2,7 +2,7 @@ require "test_helper"
 
 class SpecialtiesControllerTest < ActionDispatch::IntegrationTest
   setup do
-#    @specialty = specialties(:one)
+    @specialty = specialties(:one)
   end
 #ActionView::Template::Error: Permission denied @ rb_file_s_rename - (C:/Users/Derrin/github/SD_Project_Website/tmp/cache/assets/sprockets/v4.0.0/Xk/XkT9JgZnbi28x71xQl3gcSH8tdp2-t9u5YgNC-3qTkQ.cache.1140684898820.14408.541039, C:/Users/Derrin/github/SD_Project_Website/tmp/cache/assets/sprockets/v4.0.0/Xk/XkT9JgZnbi28x71xQl3gcSH8tdp2-t9u5YgNC-3qTkQ.cache)
 #  test "should get new" do
@@ -11,11 +11,11 @@ class SpecialtiesControllerTest < ActionDispatch::IntegrationTest
 #  end
 
   test "should create specialty" do
-#    assert_difference('Specialty.count') do
-#      post specialties_url, params: { specialty: { SpecialtyName: @specialty.SpecialtyName } }
-#    end
+    assert_difference('Specialty.count') do
+      post specialties_url, params: { specialty: { SpecialtyName: @specialty.SpecialtyName } }
+    end
 
-#    assert_redirected_to specialty_url(Specialty.last)
+    assert_redirected_to specialty_url(Specialty.last)
   end
 
   test "should show specialty" do
