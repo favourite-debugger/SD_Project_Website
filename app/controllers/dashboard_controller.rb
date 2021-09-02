05d946a1dca8057691dfa2e9c94c7d17cb22f2d3
favourite-debugger <favourobagbuwa@yahoo.com>
2021-09-02 22:36:20 +0200

    fixed javascript variable error

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -3,5 +3,10 @@ class DashboardController < ApplicationController
     @hospitals = Hospital.all
     @specialties = Specialty.all
     @allocations = Allocation.all
+
+    if (params[:hospital_id])
+      @current_hospital_id =  params[:hospital_id]
+  #else
+  end
   end
 end
